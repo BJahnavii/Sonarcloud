48e68e92fa07309169d451d4a04326d9ea8e3124
jahnavibo <jahnavibo@10.4.33.52>
2023-10-12 11:26:00 -1100

    commit1

---
+++ src/main/java/javaprgm.java
@@ -0,0 +1,13 @@
+
+public class javaprgm {
+	public String b;
+
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public String a;
+		System.out.println(a);
+
+}
+}
